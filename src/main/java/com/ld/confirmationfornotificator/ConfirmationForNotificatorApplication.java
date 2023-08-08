23c1da6fdb034fb16174d10611085c9299b992b0
Lazarev.D <Lazarev.d.g@yandex.ru>
2023-08-08 21:36:08 +0300

    C1 - Added initial project structure.

---
+++ src/main/java/com/ld/confirmationfornotificator/ConfirmationForNotificatorApplication.java
@@ -0,0 +1,13 @@
+package com.ld.confirmationfornotificator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfirmationForNotificatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfirmationForNotificatorApplication.class, args);
+	}
+
+}
